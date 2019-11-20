c58b476e20e2bed183670274fa158628e84093ea
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2019-11-20 15:30:38 +0000

    Added Health System
    Added Health System for Enemy, Player and Companion

---
+++ AINT354 Project/Assets/_Scripts/CompanionHealth.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public int regen = 5;
+
+    bool isLimping;
+    bool isRegen;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLimping && currentHealth != startingHealth && !isRegen)
+        {
+            StartCoroutine(RegenHealth());
+        }
+
+        isLimping = false;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if(isLimping)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if(currentHealth <= 10)
+        {
+            Limp();
+        }
+    }
+
+    void Limp()
+    {
+        isLimping = true;
+    }
+
+    private IEnumerator RegenHealth()
+    {
+        isRegen = true;
+        while (currentHealth < startingHealth)
+        {
+            Regen();
+            yield return new WaitForSeconds(1);
+        }
+        isRegen = false;
+    }
+
+    void Regen()
+    {
+        currentHealth += regen;
+    }
+}
---
+++ AINT354 Project/Assets/_Scripts/EnemyHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    bool isDead;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if(isDead)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if(currentHealth <=0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        Destroy(gameObject, 2f);
+    }
+}
