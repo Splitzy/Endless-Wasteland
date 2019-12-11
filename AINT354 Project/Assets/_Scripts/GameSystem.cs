c3670a0ba27b5ac26c8ba3aece1253f85bfff379
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2019-12-11 12:44:32 +0000

    Started work on Wave System

--- AINT354 Project/Assets/_Scripts/GameSystem.cs
+++ AINT354 Project/Assets/_Scripts/GameSystem.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GameSystem : MonoBehaviour
 {
+    GameObject[] enemies;
+
+    public GameObject[] waves;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        waves[0].SetActive(true);
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        int i = enemies.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        int i = enemies.Length;
+
+        if (i == 0)
+        {
+            if (waves[0].activeSelf == true)
+            {
+                waves[0].SetActive(false);
+                waves[1].SetActive(true);
+            }
+
+        }
+
     }
 }
